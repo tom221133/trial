a7f1027d275c7d6029ee4a53a9f7d750adadf6bb
tom <tom@LAPTOP-CCGDTM88>
2018-10-31 00:33:13 +0800

    first

---
+++ src/main/java/com/ntu/comsec/App.java
@@ -0,0 +1,12 @@
+package com.ntu.comsec;
+
+import org.apache.struts2.*;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+//        Gson gson = new Gson();
+        System.out.println("CVE1");
+    }
+}
